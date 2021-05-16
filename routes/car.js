122778e217807f164c6b4f705a38c998bad7f435
Tanay Raj <tanay.raj76@gmail.com>
2021-05-16 21:45:26 +0530

    car route add

---
+++ routes/car.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var Owner = require('../model/owner');
+var middleware = require('../middleware');
+
+//Index Route
+router.get('/', function (req, res) {
+  //get campgrounds from DB
+  Owner.find({ category: 'Car' }, function (err, allCar) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('laptoppage', { Owner: allCar });
+    }
+  });
+});
+//SHOW - show more info
+router.get('/:id', function (req, res) {
+  Owner.findById(req.params.id, function (err, foundCar) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('lapdesc', { Owner: foundCar });
+    }
+  });
+});
+//DESTROY Shop
+router.post('/:id', function (req, res) {
+  Owner.findByIdAndRemove(req.params.id, function (err) {
+    if (err) {
+      res.redirect('/car');
+    } else {
+      res.redirect('/car');
+    }
+  });
+});
+
+module.exports = router;
